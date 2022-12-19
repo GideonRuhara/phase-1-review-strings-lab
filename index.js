80ad12e430dda8f40064a03413b660c6d3de1b81
Gideon Ruhara <ruharagideon@gmail.com>
2022-12-19 16:18:51 +0300

    resubmission

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-// Write your code in this file!
-let currentUser = 'string'
- let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + "!"
- let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!'
-  let firstInitial = currentUser[0];
- let shortGreeting = 'Welcome, ' + firstInitial + '!'
+const currentUser = 'Gideon';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage = `Welcome to Flatbook, ${currentUser}!`.toUpperCase()
+
+const firstInitial = currentUser[0]
+const restOfName = currentUser.slice(1)
+
+const shortGreeting = `Welcome, ${firstInitial}!`
 
 
 
